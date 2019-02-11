8b803b3980b71a45eef5119df14930bbc453ad1e
Anwesh43 <anweshthecool0@gmail.com>
2019-02-11 10:22:20 +0530

    adding DAFRNode class

--- doublearcfillrotview/src/main/java/com/anwesh/uiprojects/doublearcfillrotview/DoubleArcFillRotView.kt
+++ doublearcfillrotview/src/main/java/com/anwesh/uiprojects/doublearcfillrotview/DoubleArcFillRotView.kt
@@ -120,4 +120,48 @@ class DoubleArcFillRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DAFRNode(var i : Int, val state : State = State()) {
+
+        private var next : DAFRNode? = null
+        private var prev : DAFRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = DAFRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDAFRNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DAFRNode {
+            var curr : DAFRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
