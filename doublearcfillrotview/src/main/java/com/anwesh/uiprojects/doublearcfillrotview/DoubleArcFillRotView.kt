abe0a3cd5b7724ccdd07b8a53cddbde1b77f16ec
Anwesh43 <anweshthecool0@gmail.com>
2019-02-11 10:12:38 +0530

    adding drawDAFRNode canvas extension method

--- doublearcfillrotview/src/main/java/com/anwesh/uiprojects/doublearcfillrotview/DoubleArcFillRotView.kt
+++ doublearcfillrotview/src/main/java/com/anwesh/uiprojects/doublearcfillrotview/DoubleArcFillRotView.kt
@@ -21,10 +21,37 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.8f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#212121")
+val offsetDeg : Float = 60f
+val maxDeg : Float = 360f
+val rotDeg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
-fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawDAFRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sweepDeg : Float = maxDeg / arcs
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(gap * (i + 1), h / 2)
+    rotate(rotDeg * sc2)
+    paint.style = Paint.Style.STROKE
+    drawCircle(0f, 0f, size, paint)
+    paint.style = Paint.Style.FILL
+    for (j in 0..(arcs - 1)) {
+        val sc : Float = sc1.divideScale(j, 2)
+        drawArc(RectF(-size, -size, size, size), sweepDeg * j + offsetDeg, offsetDeg * sc, true, paint)
+    }
+    restore()
+}
